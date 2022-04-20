b94c05189d7b78e4e88929d5a56996c1643056e1
David Collado Sela <david.collado@wallapop.com>
2022-04-20 19:27:45 +0200

    Experiment

--- setup/TemplateConfigurator.rb
+++ setup/TemplateConfigurator.rb
@@ -69,41 +69,19 @@ module Pod
 
     def run
       @message_bank.welcome_message
-
-      platform = self.ask_with_answers("What platform do you want to use?", ["iOS", "macOS"]).to_sym
-
-      case platform
-        when :macos
-          ConfigureMacOSSwift.perform(configurator: self)
-        when :ios
-          framework = self.ask_with_answers("What language do you want to use?", ["Swift", "ObjC"]).to_sym
-          case framework
-            when :swift
-              ConfigureSwift.perform(configurator: self)
-
-            when :objc
-              ConfigureIOS.perform(configurator: self)
-          end
-      end
-
+      ConfigureSwift.perform(configurator: self)
       replace_variables_in_files
       clean_template_files
       rename_template_files
       add_pods_to_podfile
       customise_prefix
       rename_classes_folder
-      ensure_carthage_compatibility
-      reinitialize_git_repo
       run_pod_install
 
       @message_bank.farewell_message
     end
 
     #----------------------------------------#
-
-    def ensure_carthage_compatibility
-      FileUtils.ln_s('Example/Pods/Pods.xcodeproj', '_Pods.xcodeproj')
-    end
 
     def run_pod_install
       puts "\nRunning " + "pod install".magenta + " on your new library."
@@ -179,12 +157,6 @@ module Pod
 
     def rename_classes_folder
       FileUtils.mv "Pod", @pod_name
-    end
-
-    def reinitialize_git_repo
-      `rm -rf .git`
-      `git init`
-      `git add -A`
     end
 
     def validate_user_details
